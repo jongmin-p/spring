7e2937f3536752691f4e9287cfded1f4cc86496a
jongmin-p <sjttk123@gmail.com>
2024-02-11 13:28:21 -0500

    스프링으로 전환

--- KYH/core/src/main/java/hello/core/AppConfig.java
+++ KYH/core/src/main/java/hello/core/AppConfig.java
@@ -8,22 +8,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();        // 이 부분만 바꿔주면 됨
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        // return new FixDiscountPolicy();             // 이 부분만 바꿔주면 됨
+        // return new FixDiscountPolicy();           // 이 부분만 바꿔주면 됨
         return new RateDiscountPolicy();             // 이 부분만 바꿔주면 됨
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());      // 여기서 콜함
     }
--- KYH/core/src/main/java/hello/core/MemberApp.java
+++ KYH/core/src/main/java/hello/core/MemberApp.java
@@ -3,14 +3,19 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     // 이 코드는 순수한 Java 코드.   스프링 관련된 코드 없음.
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+
+        // 스프링으로 전환
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);   // 1. 저장할 멤버 데이터 생성 (등급은 VIP로)
         memberService.join(member);                                       // 2. 생성한 멤버 데이터 넣기 (회원가입)
