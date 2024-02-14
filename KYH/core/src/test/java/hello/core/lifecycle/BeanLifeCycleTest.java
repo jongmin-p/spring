7e32a18300bbfa305a24d86bfc93ced5cf5f4dfd
jongmin-p <sjttk123@gmail.com>
2024-02-14 15:58:23 -0500

    빈 생명주기 콜백 - 시작

---
+++ KYH/core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,31 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);      // 빈 등록
+
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();     // ApplicationContext 에는 close() 가 없어서, 위에선 ConfigurableApplicationContext 사용함
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean   // 1. 빈 생성 (@Bean)
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+
+            return networkClient;       // 2. 호출된 결과물(return networkClient) 이 스프링 빈으로 등록됨.
+        }
+    }
+}
---
+++ KYH/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,32 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+
+    private String url;     // 접속해야 할 url
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    // url 은 외부에서 넣을 수 있도록 setter 이용
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스를 시작할 때 호출하는 메서드
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    // 연결이 된 후, 연결한 서버에 던질 메세지
+    public void call(String message) {
+        System.out.println("call: " + url + ", message = " + message);
+    }
+
+    // 서비스 종료 시 호출할 메서드
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+}
