a7808997be2dd33d2b51057d9bd90050454c64d7
jongmin-p <sjttk123@gmail.com>
2024-03-06 15:15:24 -0500

    Model 추가 - v3

--- KYH/servlet/src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
+++ KYH/servlet/src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -17,7 +17,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 // URL 경로 중요 (urlPatterns = "/front-controller/v3/*")
-    // v3 하위의 어떤 URL 이 들어와도, 일단 이 서블릿이 호출됨.
+// v3 하위의 어떤 URL 이 들어와도, 일단 이 서블릿이 호출됨.
 @WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
 public class FrontControllerServletV3 extends HttpServlet {
 
@@ -25,13 +25,13 @@ public class FrontControllerServletV3 extends HttpServlet {
     private Map<String, ControllerV3> controllerMap = new HashMap<>();  // 여기서 key(String) 는 URL
 
     // 매핑(Mapping) 정보를 생성자에 담아둘거임. (이 서블릿이 생성될 때 세팅되도록)
-        // 근데 new FrontControllerServletV3() 하는 곳이 있어야 생성자 내부 코드가 실행되는 것 아님?
-        // new FrontControllerServletV3() 한 곳이 없는데, 생성자가 1번 호출됨. (이유가 뭐지?)
-            // -> 그냥, HttpServlet 를 상속 받아야 하는 서블릿에서는 기본 생성자가 반드시 있어야 하고,
-                                                            // 이 기본 생성자는 자동으로 1번 실행되는 듯 (이게 서블릿 규칙)
+    // 근데 new FrontControllerServletV3() 하는 곳이 있어야 생성자 내부 코드가 실행되는 것 아님?
+    // new FrontControllerServletV3() 한 곳이 없는데, 생성자가 1번 호출됨. (이유가 뭐지?)
+    // -> 그냥, HttpServlet 를 상속 받아야 하는 서블릿에서는 기본 생성자가 반드시 있어야 하고,
+    // 이 기본 생성자는 자동으로 1번 실행되는 듯 (이게 서블릿 규칙)
     public FrontControllerServletV3() {
         System.out.println("생성자 호출 - FrontControllerServletV333");           // 서버 실행 시, 1번 호출됨. (이 생성자가 실행됐다는 의미. 이유 모름)
-            // -> 그냥, HttpServlet 를 상속 받아야 하는 서블릿에서는 반드시 기본 생성자가 있어야 하고, 이 기본 생성자는 자동으로 1번 실행되는 듯 (이게 서블릿 규칙)
+        // -> 그냥, HttpServlet 를 상속 받아야 하는 서블릿에서는 반드시 기본 생성자가 있어야 하고, 이 기본 생성자는 자동으로 1번 실행되는 듯 (이게 서블릿 규칙)
 
                                 // ┌> 얘가 호출이 되면,                        ┌> 얘가 실행됨
         controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
@@ -67,12 +67,12 @@ public class FrontControllerServletV3 extends HttpServlet {
         String viewName = mv.getViewName();         // 논리 이름인 new-form 반환 받음
 
         // 4-2. 아래의 코드는 논리 이름인 new-form 을 가지고 viewResolver 메서드 (따로 만든 메서드) 를 호출함
-                // 그러면, new MyView("/WEB-INF/views/" + viewName + ".jsp"); 를 반환해 줌.
-                // 즉, 실제 물리 이름인 "/WEB-INF/views/new-form.jsp" 이렇게 만들어 줌.
+        // 그러면, new MyView("/WEB-INF/views/" + viewName + ".jsp"); 를 반환해 줌.
+        // 즉, 실제 물리 이름인 "/WEB-INF/views/new-form.jsp" 이렇게 만들어 줌.
         MyView view = viewResolver(viewName);
 
         // 5. render() 를 호출하면서 모델도 같이 넘겨줘야 함.   왜? view 가 렌더링 하려면 Model 정보가 필요함.
-            // MyView.java 에다가, 파라미터 Map<String, Object> model 이 추가된 public void render() 를 하나 더 추가해야 함.
+        // MyView.java 에다가, 파라미터 Map<String, Object> model 이 추가된 public void render() 를 하나 더 추가해야 함.
         view.render(mv.getModel(), request, response);
     }
 
