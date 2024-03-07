c078fac9d3d78aa8f9b8c33a5e985387d77f584d
jongmin-p <sjttk123@gmail.com>
2024-03-07 15:57:23 -0500

    스프링 MVC - 시작하기

---
+++ KYH/servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,34 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+
+    // 싱글톤이라 new 안 됨.       (저장하기 위해선 MemberRepository 가 필요함)
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        // mv.getModel().put("member", member);
+        mv.addObject("member", member);
+
+        return mv;
+    }
+}
